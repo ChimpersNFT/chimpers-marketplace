2c2e493b070f36fa503f1f9e5f3a4fe10518aef7
Nick Spiers <ns@hqoo.com>
2022-05-31 13:18:07 +0000

    Expiration presets

--- components/ListModal.tsx
+++ components/ListModal.tsx
@@ -391,16 +391,34 @@ const expirationPresets = [
     display: '1 Hour',
   },
   {
+    preset: 'oneDay',
+    value: () =>
+      DateTime.now().plus({ days: 1 }).toMillis().toString().slice(0, -3),
+    display: '1 Day',
+  },
+  {
+    preset: 'threeDays',
+    value: () =>
+      DateTime.now().plus({ days: 3 }).toMillis().toString().slice(0, -3),
+    display: '3 Days',
+  },
+  {
     preset: 'oneWeek',
     value: () =>
       DateTime.now().plus({ weeks: 1 }).toMillis().toString().slice(0, -3),
-    display: '1 Week',
+    display: '7 Days',
   },
   {
-    preset: 'oneMonth',
+    preset: 'threeMonths',
     value: () =>
-      DateTime.now().plus({ months: 1 }).toMillis().toString().slice(0, -3),
-    display: '1 Month',
+      DateTime.now().plus({ months: 3 }).toMillis().toString().slice(0, -3),
+    display: '3 Months',
+  },
+  {
+    preset: 'sixMonths',
+    value: () =>
+      DateTime.now().plus({ months: 6 }).toMillis().toString().slice(0, -3),
+    display: '6 Months',
   },
   {
     preset: 'none',
--- lib/offerExpirationPresets.ts
+++ lib/offerExpirationPresets.ts
@@ -14,10 +14,28 @@ const expirationPresets = [
     display: '1 Day',
   },
   {
+    preset: 'threeDays',
+    value: () =>
+      DateTime.now().plus({ days: 3 }).toMillis().toString().slice(0, -3),
+    display: '3 Days',
+  },
+  {
     preset: 'oneWeek',
     value: () =>
       DateTime.now().plus({ weeks: 1 }).toMillis().toString().slice(0, -3),
-    display: '1 Week',
+    display: '7 Days',
+  },
+  {
+    preset: 'threeMonths',
+    value: () =>
+      DateTime.now().plus({ months: 3 }).toMillis().toString().slice(0, -3),
+    display: '3 Months',
+  },
+  {
+    preset: 'sixMonths',
+    value: () =>
+      DateTime.now().plus({ months: 6 }).toMillis().toString().slice(0, -3),
+    display: '6 Months',
   },
   {
     preset: 'none',
