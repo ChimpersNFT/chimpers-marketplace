12232252422306fd2127ebdf79a4e2aba8fdf154
Captain Bad <cba@freshdrops.io>
2022-06-03 00:39:11 -0400

    Improvements to theme switcher

--- components/ThemeSwitcher.tsx
+++ components/ThemeSwitcher.tsx
@@ -7,25 +7,31 @@ const ThemeSwitcher = () => {
         <ToggleGroup.Root
             type="single"
             defaultValue="grid"
-            aria-label="Change view"
+            aria-label="Change theme"
             className="border flex divide-x-[1px] divide-[#D1D5DB] overflow-hidden rounded-[8px] border-[1px] border-[#D1D5DB] dark:divide-neutral-600 dark:border-neutral-600 md:none"
         >
             <ToggleGroup.Item
                 onClick={() => setTheme('light')}
-                value="grid"
-                // disabled={!!router.query?.view && !router.query?.view}
-                className="block select-none p-3 transition flex-auto"
+                value="light"
+                disabled={theme == "light"}
+                className={`block select-none p-3 transition flex-auto ${
+                    theme == "light"
+                      ? 'cursor-not-allowed bg-[#F1E5FF] dark:bg-primary-900'
+                      : 'hover:bg-[#F1E5FF] dark:hover:bg-primary-900'
+                  }`}
                 aria-label="Light Mode"
             >
                 <div className='sun dark:sun-dark mx-auto'></div>
             </ToggleGroup.Item>
             <ToggleGroup.Item
                 onClick={() => setTheme('dark')}
-                value="table"
-                // disabled={
-                //     !!router.query?.view && router.query?.view.toString() === 'table'
-                // }
-                className="block select-none p-3 transition flex-auto"
+                value="dark"
+                disabled={theme == "dark"}
+                className={`block select-none p-3 transition flex-auto ${
+                    theme == "dark"
+                      ? 'cursor-not-allowed bg-[#F1E5FF] dark:bg-primary-900'
+                      : 'hover:bg-[#F1E5FF] dark:hover:bg-primary-900'
+                  }`}
                 aria-label="Dark Mode"
             >
                 <div className='moon dark:moon-dark mx-auto'></div>
